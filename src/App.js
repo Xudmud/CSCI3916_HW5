b8661cb4975e75f8b962b33900d78bf9199aac6b
Xudmud <morichikarinnosuke17@gmail.com>
2019-04-03 18:01:30 -0600

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p> No really, you need to edit <code>src/App.js</code> and save to reload.</p>
           <a
             className="App-link"
             href="https://reactjs.org"

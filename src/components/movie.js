8dd67cccc18ca34ef88acf0b30dea57143bf7c83
Xudmud <morichikarinnosuke17@gmail.com>
2019-05-04 02:46:13 -0600

    Possibly add review support

--- src/components/movie.js
+++ src/components/movie.js
@@ -4,8 +4,117 @@ import { Glyphicon, Panel, ListGroup, ListGroupItem } from 'react-bootstrap'
 import { Image } from 'react-bootstrap'
 import { withRouter } from "react-router-dom";
 import {fetchMovie} from "../actions/movieActions";
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+import {Col, Form, FormGroup, FormControl, Button, ControlLabel } from 'react-bootstrap';
+import qs from 'qs';
 
 //support routing by creating a new component
+
+//Add new review
+class ReviewInput extends Component {
+    constructor(props) {
+        super(props);
+        this.submitReview = this.submitReview.bind(this);
+        this.updateDetails = this.updateDetails.bind(this);
+
+        this.state = {
+            details: {
+                rating: 5,
+                review: ""
+            }
+        }
+    }
+
+    submitReview() {
+        const env = runtimeEnv();
+        let details = {
+            'movieId': this.props.movieId,
+            'review': this.state.details.review,
+            'rating': parseInt(this.state.details.rating)
+        };
+        let formBody = [];
+        for (let property in details) {
+            let encodedKey = encodeURIComponent(property);
+            let encodedValue = encodeURIComponent(details[property]);
+            formBody.push(encodedKey + "=" + encodedValue);
+        }
+        formBody = formBody.join("&");
+
+        let headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-urlencoded',
+            'Authorization': localStorage.getItem('token')
+        };
+        console.log("headers: "+JSON.stringify(headers));
+        console.log("body: "+JSON.stringify(formBody));
+        return fetch(`${env.REACT_APP_API_URL}/reviews`, {
+            method: 'POST',
+            mode: 'cors',
+            headers: headers,
+            body: formBody
+        })
+            .then((response) => {
+                console.log("response: " + JSON.stringify(response));
+                console.log("statusText: " + response.statusText);
+                if (!response || !response.status) {
+                    throw Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then((res) => {
+                console.log("res: " + JSON.stringify(res));
+                //window.location.reload();
+            })
+            .catch((e) => console.log(e));
+
+    }
+
+    updateDetails(event) {
+        let updateDetails = Object.assign({},this.state.details);
+        updateDetails[event.target.id] = event.target.value;
+        this.setState({
+            details: updateDetails
+        });
+    }
+
+    render() {
+        return(
+            <Form horizontal>
+                <h3>Write a Review</h3>
+                <FormGroup controlId="rating">
+                    <Col componentClass={ControlLabel} sm={2}>
+                        Rating
+                    </Col>
+                    <Col sm={10}>
+                        <FormControl onChange={this.updateDetails}
+                                     componentClass={"select"}
+                                     placeholder={"rating"}
+                                     value={this.state.details.rating}>
+                        <option value={"5"}>&#9733;&#9733;&#9733;&#9733;&#9733;</option>
+                        <option value={"4"}>&#9733;&#9733;&#9733;&#9733;</option>
+                        <option value={"3"}>&#9733;&#9733;&#9733;</option>
+                        <option value={"2"}>&#9733;&#9733;</option>
+                        <option value={"1"}>&#9733;</option>
+                        </FormControl>
+                    </Col>
+                </FormGroup>
+                <FormGroup controlId={"review"}>
+                    <Col componentClass={ControlLabel} sm={2}>
+                        Review
+                    </Col>
+                    <Col sm={10}>
+                        <FormControl onChange={this.updateDetails}
+                                     value={this.state.details.review} componentClass={"textarea"}
+                                     placeholder={"Type your review here."} />
+                    </Col>
+                </FormGroup>
+                <FormGroup>
+                    <Button onClick={this.submitReview}>Submit</Button>
+                </FormGroup>
+            </Form>
+        )
+    }
+}
 
 class Movie extends Component {
 
@@ -31,7 +140,8 @@ class Movie extends Component {
                     <Glyphicon glyph={'star'} /> {review.rating}
                 </p>
             );
-        }
+        };
+
 
         const DetailInfo = ({currentMovie}) => {
             if (!currentMovie) { // evaluates to true if currentMovie is null
@@ -47,6 +157,10 @@ class Movie extends Component {
                         <ListGroupItem><h4><Glyphicon glyph={'star'} /> {currentMovie.avgRating} </h4></ListGroupItem>
                     </ListGroup>
                     <Panel.Body><ReviewInfo reviews={currentMovie.reviews} /></Panel.Body>
+                    <Panel.Body>
+                        <h2>Add a review</h2>
+                        <ReviewInput movieId={currentMovie._id} />
+                    </Panel.Body>
                 </Panel>
             );
         };
